35abf82b01d6bb75f20130b3b15e72ec7e6b5dfb
Andrew Skrypnyk <a.g.skryp@gmail.com>
2017-03-08 23:06:42 -0500

    Image link test #3

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@
         '/Images/Background---Black-Ice.jpg',
         '/Images/Background---Colour-Storm.jpg',
         '/Images/Background---Electric-Water.jpg',
-        '../Images/Background---Lime-Drops.jpg',
-        '../Images/Background---Natural-Bubbles.jpg',
-        '../Images/Background---Nice-Try.jpg',
+        '../../Images/Background---Lime-Drops.jpg',
+        '../../Images/Background---Natural-Bubbles.jpg',
+        '../../Images/Background---Nice-Try.jpg',
         '../Images/Background---Peach-Hexagon.jpg',
         '../Images/Background---Room-303.jpg'
     ];
