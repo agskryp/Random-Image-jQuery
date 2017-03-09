3c9db0ba39371ddd4de5c5f6e702bafd56cdbcf3
Andrew Skrypnyk <a.g.skryp@gmail.com>
2017-03-09 13:08:23 -0500

    Image links fixed
    Replaced short URL code that worked on localhost with full URL.

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@
         'https://agskryp.github.io/Random-Image-jQuery/Images/Background---Electric-Water.jpg',
         'https://agskryp.github.io/Random-Image-jQuery/Images/Background---Lime-Drops.jpg',
         'https://agskryp.github.io/Random-Image-jQuery/Images/Background---Natural-Bubbles.jpg',
-        '../../Images/Background---Nice-Try.jpg',
-        '../Images/Background---Peach-Hexagon.jpg',
-        '../Images/Background---Room-303.jpg'
+        'https://agskryp.github.io/Random-Image-jQuery/Images/Images/Background---Nice-Try.jpg',
+        'https://agskryp.github.io/Random-Image-jQuery/Images/Images/Background---Peach-Hexagon.jpg',
+        'https://agskryp.github.io/Random-Image-jQuery/Images/Images/Background---Room-303.jpg'
     ];
 
     function getRandomImage(images) {
